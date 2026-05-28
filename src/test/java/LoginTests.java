b461ed85ee338c25e8cee077e24c4e6911e86890
RenataFatykhov <renata.musenova@gmail.com>
2026-05-28 16:18:26 +0300

    added before each

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,10 +1,17 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class LoginTests {
+
+    @BeforeEach
+    void setup(){
+        closeWebDriver();
+    }
+
     @Test
     void successfulAuthorizationTest() {
         open("https://qa-guru.github.io/one-page-form/login.html");
@@ -14,7 +21,7 @@ public class LoginTests {
         $("[data-testid=welcome-message]").shouldHave(text("Welcome, user1!"));
     }
     @Test
-    void successfulEnterAuthorizationTest() {
+    void successfulAuthorizationByEnterTest() {
         open("https://qa-guru.github.io/one-page-form/login.html");
         $("[data-testid=login-input]").setValue("user1");
         $("[data-testid=password-input]").setValue("password1").pressEnter();
@@ -57,7 +64,7 @@ public class LoginTests {
         $("[data-testid=error-message]").shouldHave(text("Password is required (minimum 6 characters)"));
     }
     @Test
-    void emptyPasswordEnterAuthorizationTest() {
+    void emptyPasswordAuthorizationByEnterTest() {
         open("https://qa-guru.github.io/one-page-form/login.html");
         $("[data-testid=login-input]").setValue("user1").pressEnter();
         $("[data-testid=error-message]").shouldHave(text("Password is required (minimum 6 characters)"));
@@ -66,6 +73,23 @@ public class LoginTests {
     void emptyAuthorizationTest() {
         open("https://qa-guru.github.io/one-page-form/login.html");
         $("[data-testid=submit-button]").click();
-        $("[data-testid=error-message]").shouldHave(text("Login and password are required (minimum 3 and 6 characters)"));
+        $("[data-testid=error-message]").shouldHave(text(
+                "Login and password are required (minimum 3 and 6 characters)"));
+    }
+    @Test
+    void shortLoginAuthorizationTest() {
+        open("https://qa-guru.github.io/one-page-form/login.html");
+        $("[data-testid=login-input]").setValue("us");
+        $("[data-testid=password-input]").setValue("password1");
+        $("[data-testid=submit-button]").click();
+        $("[data-testid=error-message]").shouldHave(text("Login must be at least 3 characters"));
+    }
+    @Test
+    void shortPasswordAuthorizationTest() {
+        open("https://qa-guru.github.io/one-page-form/login.html");
+        $("[data-testid=login-input]").setValue("user1");
+        $("[data-testid=password-input]").setValue("p");
+        $("[data-testid=submit-button]").click();
+        $("[data-testid=error-message]").shouldHave(text("Password must be at least 6 characters"));
     }
 }
